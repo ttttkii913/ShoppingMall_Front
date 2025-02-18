5769d7ebbe12ffdc091124b54b765f885dc1471b
ttttkii913 <dbgkdud821@daum.net>
2025-02-19 02:47:16 +0900

    ✨ Feat: Navbar Navigate

--- shoppingmall/src/components/navigator/Navbar.jsx
+++ shoppingmall/src/components/navigator/Navbar.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import "./Navbar.css";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleNavigation = (path) => {
+    navigate(path);
+  };
+
   return (
     <div className="navbar">
-      <div className="navbar-item">
+      <div className="navbar-item" onClick={() => handleNavigation("/")}>
         <div>홈</div>
       </div>
-      <div className="navbar-item">
+      <div className="navbar-item" onClick={() => handleNavigation("/category")}>
         <div>카테고리</div>
       </div>
-      <div className="navbar-item">
+      <div className="navbar-item" onClick={() => handleNavigation("/like")}>
         <div>좋아요</div>
       </div>
-      <div className="navbar-item">
+      <div className="navbar-item" onClick={() => handleNavigation("/login")}>
         <div>마이</div>
       </div>
     </div>
